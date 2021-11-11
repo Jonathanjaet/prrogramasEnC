bf42a0b87b83cbfba84d3e95cb41d0f35998dac2
Jonathanjaet <79382765+Jonathanjaet@users.noreply.github.com>
2021-11-10 21:25:01 -0600

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,30 @@
+/* Este programa intercambia 2 valores
+ * ingresados por el susuario cuando
+ * se cumple la condición de que sean pares
+ */
+
+//Librerias
+#include <stdio.h>
+
+//Fundiones
+int swap(int,int);//Funcion de intercambio de valores
+
+//Variables globales
+int num1=0, num2=0;
+
+#include <stdio.h>
+
+int main(){
+        int resultado=0, contador=0;
+        printf("Ingrese el primer valor...: ");
+        scanf("%d",&num1);
+        resultado = num1 % 2;
+        if (resultado == 0){
+                contador = contador + 1;
+        }
+
+        printf("\n");
+        printf("Ingrese el segundo valor...: ");
+        scanf("%d",&num2);
+        resultado
+}
